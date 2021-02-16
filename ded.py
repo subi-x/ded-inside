de41598ec3e98b7fd4f9294110da34c18f3636e9
subi-x <79165322+subi-x@users.noreply.github.com>
2021-02-16 21:37:30 +0300

    Add files via upload

---
+++ ded.py
@@ -0,0 +1,13 @@
+import time
+x = 1000;
+
+print("El PrImO");
+time.sleep(1);
+print("БрА бРа бРаВл СтАрс")
+time.sleep(1);
+
+while x != 0:
+    print("1000-7=",x)
+    x=x-1;
+    time.sleep(0.1);
+print("zxc дед инсайд хавыхпвахпыхх");
